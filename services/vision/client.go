850ec1db52b66f41c763f457d3cb43f784e0ba60
Khari Jarrett <khari@viam.com>
2023-08-30 11:04:09 -0400

    blah

--- services/vision/client.go
+++ services/vision/client.go
@@ -226,7 +226,7 @@ func protoToObjects(pco []*commonpb.PointCloudObject) ([]*vision.Object, error) 
 			}
 			return ""
 		}()
-		n := len(o.Geometries.GetGeometries())
+		n := len(pco)
 		fmt.Printf("N: %v\n", n)
 		if i < n {
 			objects[i], err = vision.NewObjectWithLabel(pc, label, o.Geometries.GetGeometries()[i])
